4b4f2f77d10831dcefdad4a96979807aaa710b62
dlathigara <85980421+dlathigara@users.noreply.github.com>
2021-07-29 13:23:29 +0530

    Create test_ps_script.ps1

---
+++ test_ps_script.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+param ($input1)
+Write-Host "This is a demo Powershell script."
+Write-Host "Value Passed for parameter is:" $input1
+Write-Host "Over and out."
